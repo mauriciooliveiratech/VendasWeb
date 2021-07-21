31daaaeffbc718d01b05b417fe0875e6cc0bfb13
mauriciooliveiratech <mauricioazol@gmail.com>
2021-07-21 10:24:02 -0300

    Inicio do Crud

---
+++ Controllers/DepartmentsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VendasWeb.Models;
+
+namespace VendasWeb.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> list = new List<Department>();
+            list.Add(new Department { Id = 1, Name = "Eletronicos" });
+            list.Add(new Department { Id = 1, Name = "Roupas" });
+            list.Add(new Department { Id = 1, Name = "Móveis" });
+            list.Add(new Department { Id = 1, Name = "Alimentos" });
+
+            return View(list);
+        }
+    }
+}
